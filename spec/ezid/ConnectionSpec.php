68f195fa57bce9ecfa71e2ef16698dfc9b1ba4c8
Robert Olendorf <drolendorf@gmail.com>
2016-03-24 16:50:50 +0000

    basic config setup

--- spec/ezid/ConnectionSpec.php
+++ spec/ezid/ConnectionSpec.php
@@ -9,6 +9,8 @@ class ConnectionSpec extends ObjectBehavior
 {
     const DEFAULT_URL = 'ezid.cdlib.org';
     
+    
+    
     function it_is_initializable()
     {
         $this->shouldHaveType('ezid\Connection');
@@ -17,5 +19,20 @@ class ConnectionSpec extends ObjectBehavior
     function it_has_the_right_default_url()
     {
         $this->url->shouldEqual($this::DEFAULT_URL);
+    }
+    
+    function it_should_initialize_from_config()
+    {
+        $config = json_decode(file_get_contents("config/ezid.json"), true);
+        $this->username->shouldEqual($config["username"]);
+        $this->password->shouldEqual($config["password"]);
+    }
+    
+    function it_should_override_config_with_constructor_options()
+    {
+        $opts = array("username"=>"john_doe", "password"=>"foobaz");
+        $this->beConstructedWith($opts);
+        $this->username->shouldEqual($opts["username"]);
+        $this->password->shouldEqual($opts["password"]);
     }
 }
--- src/ezid/Connection.php
+++ src/ezid/Connection.php
@@ -8,9 +8,14 @@ class Connection
     const DEFAULT_URL = 'ezid.cdlib.org';
     
     public $url;
+    public $username;
+    public $password;
     
-    public function __construct()
+    public function __construct($opts = null)
     {
+        $config = json_decode(file_get_contents("config/ezid.json"), true);
         $this->url = $this::DEFAULT_URL;
+        $this->username = empty($opts["username"]) ? $config["username"] : $opts["username"];
+        $this->password = empty($opts["password"]) ? $config["password"] : $opts["password"];
     }
 }
